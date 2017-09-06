be81f9ffb5dafcc5ccf81756dd7432df3d79c1f1
3255713 <3255713>
2017-09-05 20:42:19 -0500

    second commit

---
+++ firstprogram.cpp
@@ -0,0 +1,26 @@
+//******************************************************************
+// Template Program
+// Programmer: Scooby Doo
+// Completed : Sept 01, 2015
+// Status    : Complete
+//
+// This program will be used to start my project
+//******************************************************************
+
+#include <iostream>				// for cin, cout, endl
+using namespace std;
+
+int main()						
+{ 
+    int x = 5; //initialize integer x and set to 5
+    int y = 15; //initialize integer y and set to 15
+    int sum = x + y; //calculate the sum of x and y and set it to variable of sum
+
+//  Print 
+    cout << sum << endl; //send the contents of the sum variable to the output
+
+    return 0; //set a return variable equal to the datatype of main
+	           
+
+}                               // end of main function
+
