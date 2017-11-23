25b13cf6f9cab7f94d719b859743afc3cc39617a
Julie Laursen <Julielaursen1@gmail.com>
2017-11-22 18:39:48 -0600

    Finished project 8

--- Project8laursen.cpp
+++ Project8laursen.cpp
@@ -1,9 +1,9 @@
  using namespace std;
 #include <string>
-#include <iomanip>
+
 #include <iostream>
 #include <fstream>
-
+#include <iomanip>
 
 
 
@@ -16,36 +16,56 @@ class Employee
     double hourlyPay;   // pay per hour
     int numDeps;        // number of dependents
     int type;           // employee type
-    float rate;			// payrate
+    //float rate;			// payrate
+    char gender;
     
   public:
   	//public class declarations
-    Employee( int initId=0, char initName[]='\0', 
-              double initHourlyPay=0.0, 
-              int initNumDeps=0, int initType=0 );  // Constructor
+    Employee( int initId, char initName[], 
+              double initHourlyPay, 
+              int initNumDeps, int initType );  // Constructor
 
 	int getID(){return id;}	
+	char* getName(){return name;}
 	int getHours(){return hourlyPay;}	
-	int getRate(){return rate;}											///add other get functions
+	int getRate(){return hourlyPay;}											///add other get functions
 	
 	//called later with employee variables passed in
-    bool set(int newId, string newName, double newHourlyPay,
-             int newNumDeps, int newType)
-             {
-			  id = newId;
-              name = newName;
-              hours = newHourlyPay;
-			  numDeps = newNumDeps;
-              type = newType;
-			  }
+    bool set(int newId, char newName[], double newHourlyPay,
+             int newNumDeps, int newType);
+             
 };
 
-class Timecard {
-	 private:
-    	int id;             // employee ID
+class TimeCard 
+{
+	private:
+    	int id;            
     	float hours;
+    	
+    public:
+    TimeCard(){id =0; hours=0;}
+	TimeCard(int initId, float initHours);
+	
+	int getID(){return id;}
+	float getHours(){return hours;}
+	
+	bool set(int initId, float initHours);
 };
-
+bool TimeCard::set(int initId, float initHours)
+{
+	if(initHours < 0)
+	{
+		id =0;
+		hours=0;
+		return false;
+	}
+	else
+	{
+		id =initId;
+		hours=initHours;
+		return true;
+	}
+}
 //call employee function using class employee
 Employee::Employee( int initId=0, char initName[]='\0', 
               		double initHourlyPay=0.0, 
@@ -58,7 +78,7 @@ Employee::Employee( int initId=0, char initName[]='\0',
   	if ( !status )
   	{
     	id = 0;
-    	name = '\0';
+    	name[0] = '\0';
     	hourlyPay = 0.0;
     	numDeps = 0;
     	type = 0;    
@@ -68,7 +88,7 @@ Employee::Employee( int initId=0, char initName[]='\0',
 //call set function of class employee
 bool Employee::set( int newId, char newName[], double newHourlyPay,
                                  int newNumDeps, int newType )
-  {	
+{
   //initialize status to false																				/// check error checking
   bool status = false;
   //error check that all file inputs are true
@@ -94,28 +114,8 @@ bool Employee::set( int newId, char newName[], double newHourlyPay,
   return status;
 }
 
-float payrollReport(Employee emp[], Timecard tc[], int size)					/// finish struct -> class translation
-{
-	cout << left <<setw(4) <<"ID"<<setw(27)<<"Name" <<right << setw(9) << "Gross Pay" << setw(9)  << "Tax" << setw(9) << "Net Pay" << endl;
 
-	float totalGross = 0;
-	float totalNet = 0;
-	
-	for (int i = 0; i < 4; i++ )
-	{
-		float grossPay = calcPay(employee[i], paystub, size);
-		//calc tax and net pay off of grossPay
-		cout << left <<setw(4) <<employee[i].empId<<setw(27)
-		<<employee[i].empName  <<right << setw(9) <<paystub[i].grossPay << setw(9) << paystub[i].tax << setw(9) << paystub[i].netPay << endl;
-		totalGross += tc[i].grossPay;
-		totalNet += tc[i].netPay;
-	}
-	cout << "Total Gross Pay: $" <<totalGross<< endl;
-	cout << "Total Net Pay: $" <<totalNet<< endl;
-
-}
-
-float Employee::calcPay(Employee emp, TimeCard tc[], int size)
+float calcPay(Employee emp, TimeCard tc[], int size)
 {
 	float grossPay=0;
 	for(int i=0; i<size && grossPay == 0; i++)
@@ -126,19 +126,43 @@ float Employee::calcPay(Employee emp, TimeCard tc[], int size)
 			if(hours > 40)														/// finish struct -> class translation
 			{
 				grossPay = emp.getRate() * 40;
-				grossPay += (hours - 40) * (1.5 * employee[i].payRate);
-				cout << paystub[i].grossPay;
+				grossPay += (hours - 40) * (1.5 * emp.getRate());
 			}
 			else
 			{
-				paystub[i].grossPay = employee[i].payRate * hours;
+				grossPay = emp.getRate() * hours;
 			}
 		}
 	}
 	return grossPay;
 }
 
-void popEmployees(employeeFile, emp, size)
+float payrollReport(Employee emp[], TimeCard tc[], int size)					/// finish struct -> class translation
+{
+	cout << left <<setw(4) <<"ID"<<setw(27)<<"Name" <<right << setw(9) << "Gross Pay" << setw(9)  << "Tax" << setw(12) << "Insurance" << setw(9) << "Net Pay" << endl;
+
+	float totalGross = 0;
+	float totalNet = 0;
+	
+	for (int i = 0; i < size; i++ )
+	{
+		float grossPay = calcPay(emp[i], tc, size);
+		float tax = .15 * grossPay;
+		int insurance = 30;
+		float netPay = grossPay - tax - insurance;
+		cout << left <<setw(4) <<emp[i].getID()<<setw(27)
+		<<emp[i].getName()  <<right << setw(9) <<grossPay << setw(9) << tax << setw(12) << insurance << setw(9)<< netPay << endl;
+		totalGross += grossPay;
+		totalNet += netPay;
+	}
+	cout << endl;
+	cout << "Total Gross Pay: $" <<totalGross<< endl;
+	cout << "Total Net Pay: $" <<totalNet<< endl;
+
+}
+
+
+int popEmployees(ifstream& employeeFile, Employee emp[], int size)
 {
 	//declares variables
 	int empId;
@@ -146,9 +170,11 @@ void popEmployees(employeeFile, emp, size)
 	float payRate;
 	int dependents;
 	int employeeType;
+	char gender;
+	int counter=0;
 	//gets variables from employeeFile doc
-	employeeFile >> empId;
-	for (int i = 0; i < size && empId == '\n'; i++ )  
+	//employeeFile >> empId;
+	for (int i = 0; i < size && employeeFile >> empId; i++ )  
 	{
 		
 		employeeFile.get(empName, 20, '\n');
@@ -156,24 +182,27 @@ void popEmployees(employeeFile, emp, size)
 		employeeFile >> payRate;
 		employeeFile >> dependents;
 		employeeFile >> employeeType;
+		employeeFile >> gender;
 		
 		//passes empId, empName, payRate, dependents, employeeType from the file to set function
-		emps[i].set(empId, empName, payRate, dependents, employeeType);
+		emp[i].set(empId, empName, payRate, dependents, employeeType);
+		
+		//employeeFile >> empId;
+		counter++;
 	}
+	return counter;
 }
 
-void popTimeCard(transFile, tc, size)
+void popTimeCard(ifstream& transFile, TimeCard tc [], int size)
 {
 	//declares variables
 	int empId;
-	float payRate;
-	transFile >> empId;
-	for (int i = 0; i < size && empId == '\n'; i++ )  
+	float hours;
+	for (int i = 0; i < size && transFile >> empId; i++ )  
 	{
 		transFile >> hours;
 		//passes empId, empName, payRate, dependents, employeeType from the file to set function
-		tc[i].getID(empId);
-		tc[i].getHours(hours);
+		tc[i].set(empId, hours);
 	}
 }
 
@@ -183,21 +212,21 @@ int main()
 	ifstream employeeFile;
 	int size = 20;
 	Employee emp[size];
-	Timecard tc[size];
+	TimeCard tc[size];
 	ifstream transFile;
+	int recNum;
 	
 	transFile.open("trans10.txt");
-	employeeFile.open("master10.txt");								// play with file not found logic
-	if (!employeeFile || !transFile)
+	employeeFile.open("master10.txt");								
+	if (!employeeFile)
 	{
 		cout << "Cannot open file" << endl;
 		return 1;
 	}
 	else
 	{
-		popEmployees(employeeFile, emps, size);
+		recNum = popEmployees(employeeFile, emp, size);
 	}
-		
 	
 	if (!transFile)
 	{
@@ -208,7 +237,8 @@ int main()
 	{
 		popTimeCard(transFile, tc, size);
 	}
-	payrollReport(emps, tc, size);
+	payrollReport(emp, tc, recNum);
 	
+	return 0;
 
 }
